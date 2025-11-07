787f922068fc22b47dfbd7b2a9b2993069140b80
Claus-Peter Stelzer <claus-peter.stelzer@uibk.ac.at>
2025-11-07 08:57:49 +0100

    Edits for running popgrowth_20251001 on HPC cluster

--- code/start_wc_analyse.sh
+++ code/start_wc_analyse.sh
@@ -1,20 +1,11 @@
-#!/bin/bash
+#!/bin/bash 
 
-cd "$SCRATCH/wellcounter/popgrowth_20251001"
+cd $SCRATCH/wellcounter/popgrowth_20251001
 
-dates=(
-    20251002
-    20251003
-    20251004
-    20251005
-    20251006
-    20251007
-    20251008
-    20251009
-    20251010
-)
+dates=(20251002 20251003 20251004 20251005 20251006 20251007 20251008 20251009 20251010)
 
 for date in "${dates[@]}"
 do
-   sbatch "$SCRATCH/wellcounter/popgrowth_20250627/wc_analyse_date.slrm" "$date"
+   sbatch $SCRATCH/wellcounter/popgrowth_20251001/wc_analyse_date.slrm $date 
 done
+
